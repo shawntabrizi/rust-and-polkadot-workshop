26761c2b65b64cdfb4910cec1a9f1f7e2f6499f7
Shawn Tabrizi <shawntabrizi@gmail.com>
2023-11-06 17:07:52 -0400

    fmt

--- code/src/balances.rs
+++ code/src/balances.rs
@@ -2,75 +2,65 @@ use std::collections::BTreeMap;
 
 #[derive(Debug)]
 pub struct BalancesModule {
-    balances: BTreeMap<&'static str, u128>,
+	balances: BTreeMap<&'static str, u128>,
 }
 
 pub enum BalancesCall {
-    Transfer { to: &'static str, amount: u128 },
+	Transfer { to: &'static str, amount: u128 },
 }
 
 impl BalancesModule {
-    pub fn new() -> Self {
-        Self {
-            balances: BTreeMap::new(),
-        }
-    }
+	pub fn new() -> Self {
+		Self { balances: BTreeMap::new() }
+	}
 
-    pub fn set_balance(&mut self, who: &'static str, amount: u128) {
-        self.balances.insert(who, amount);
-    }
+	pub fn set_balance(&mut self, who: &'static str, amount: u128) {
+		self.balances.insert(who, amount);
+	}
 
-    pub fn balance(&self, who: &'static str) -> u128 {
-        *self.balances.get(&who).unwrap_or(&0)
-    }
+	pub fn balance(&self, who: &'static str) -> u128 {
+		*self.balances.get(&who).unwrap_or(&0)
+	}
 
-    pub fn transfer(
-        &mut self,
-        from: &'static str,
-        to: &'static str,
-        amount: u128,
-    ) -> Result<(), &'static str> {
-        let from_balance = self.balance(&from);
-        let to_balance = self.balance(&to);
+	pub fn transfer(
+		&mut self,
+		from: &'static str,
+		to: &'static str,
+		amount: u128,
+	) -> Result<(), &'static str> {
+		let from_balance = self.balance(&from);
+		let to_balance = self.balance(&to);
 
-        let new_from_balance = from_balance
-            .checked_sub(amount)
-            .ok_or("Not enough funds.")?;
-        let new_to_balance = to_balance.checked_add(amount).ok_or("Overflow")?;
+		let new_from_balance = from_balance.checked_sub(amount).ok_or("Not enough funds.")?;
+		let new_to_balance = to_balance.checked_add(amount).ok_or("Overflow")?;
 
-        self.balances.insert(from, new_from_balance);
-        self.balances.insert(to, new_to_balance);
+		self.balances.insert(from, new_from_balance);
+		self.balances.insert(to, new_to_balance);
 
-        Ok(())
-    }
+		Ok(())
+	}
 }
 
 #[test]
 fn init_balance() {
-    let mut balances = BalancesModule::new();
+	let mut balances = BalancesModule::new();
 
-    assert_eq!(balances.balance(&"alice"), 0);
-    balances.set_balance(&"alice", 100);
-    assert_eq!(balances.balance(&"alice"), 100);
-    assert_eq!(balances.balance(&"bob"), 0);
+	assert_eq!(balances.balance(&"alice"), 0);
+	balances.set_balance(&"alice", 100);
+	assert_eq!(balances.balance(&"alice"), 100);
+	assert_eq!(balances.balance(&"bob"), 0);
 }
 
 #[test]
 fn transfer_balance() {
-    let mut balances = BalancesModule::new();
+	let mut balances = BalancesModule::new();
 
-    assert_eq!(
-        balances.transfer(&"alice", &"bob", 51),
-        Err("Not enough funds.")
-    );
+	assert_eq!(balances.transfer(&"alice", &"bob", 51), Err("Not enough funds."));
 
-    balances.set_balance(&"alice", 100);
-    assert_eq!(balances.transfer(&"alice", &"bob", 51), Ok(()));
-    assert_eq!(balances.balance(&"alice"), 49);
-    assert_eq!(balances.balance(&"bob"), 51);
+	balances.set_balance(&"alice", 100);
+	assert_eq!(balances.transfer(&"alice", &"bob", 51), Ok(()));
+	assert_eq!(balances.balance(&"alice"), 49);
+	assert_eq!(balances.balance(&"bob"), 51);
 
-    assert_eq!(
-        balances.transfer(&"alice", &"bob", 51),
-        Err("Not enough funds.")
-    );
+	assert_eq!(balances.transfer(&"alice", &"bob", 51), Err("Not enough funds."));
 }
--- code/src/main.rs
+++ code/src/main.rs
@@ -2,75 +2,89 @@ mod balances;
 mod system;
 
 enum RuntimeCall {
-    Balances(balances::BalancesCall),
+	Balances(balances::BalancesCall),
 }
 
 struct Extrinsic {
-    caller: &'static str,
-    call: RuntimeCall,
+	caller: &'static str,
+	call: RuntimeCall,
 }
 
 fn main() {
-    println!("Hello, world!");
+	// Create a new instance of the Runtime.
+	// It will instantiate with it all the modules it uses.
+	let mut runtime = Runtime::new();
 
-    let mut runtime = Runtime::new();
+	// Initialize the system with some initial balance.
+	runtime.balances.set_balance(&"alice", 100);
 
-    runtime.balances.set_balance(&"alice", 100);
+	// Here are the extrinsics in our block.
+	// You can add or remove these based on the modules and calls you have set up.
+	let extrinsics = vec![
+		Extrinsic {
+			caller: &"alice",
+			call: RuntimeCall::Balances(balances::BalancesCall::Transfer {
+				to: &"bob",
+				amount: 20,
+			}),
+		},
+		Extrinsic {
+			caller: &"alice",
+			call: RuntimeCall::Balances(balances::BalancesCall::Transfer {
+				to: &"charlie",
+				amount: 20,
+			}),
+		},
+	];
 
-    runtime
-        .execute_block(vec![
-            Extrinsic {
-                caller: &"alice",
-                call: RuntimeCall::Balances(balances::BalancesCall::Transfer {
-                    to: &"bob",
-                    amount: 20,
-                }),
-            },
-            Extrinsic {
-                caller: &"alice",
-                call: RuntimeCall::Balances(balances::BalancesCall::Transfer {
-                    to: &"charlie",
-                    amount: 20,
-                }),
-            },
-        ])
-        .expect("invalid block");
+	// Execute the extrinsics which make up our block.
+	// If there are any errors, our system panics, since we should not execute invalid blocks.
+	runtime.execute_block(extrinsics).expect("invalid block");
 
-    println!("{:#?}", runtime);
+	// Simply print the debug format of our runtime state.
+	println!("{:#?}", runtime);
 }
 
+// This is our main Runtime.
+// It accumulates all of the different modules we want to use,
+// functions implemented on the Runtime allow us to access those modules and execute blocks of
+// transactions.
 #[derive(Debug)]
 struct Runtime {
-    pub system: system::SystemModule,
-    pub balances: balances::BalancesModule,
+	pub system: system::SystemModule,
+	pub balances: balances::BalancesModule,
 }
 
 impl Runtime {
-    fn new() -> Self {
-        Self {
-            system: system::SystemModule::new(),
-            balances: balances::BalancesModule::new(),
-        }
-    }
+	// Create a new instance of the main Runtime, by creating a new instance of each module.
+	fn new() -> Self {
+		Self { system: system::SystemModule::new(), balances: balances::BalancesModule::new() }
+	}
 
-    fn execute_block(&mut self, extrinsics: Vec<Extrinsic>) -> Result<(), &'static str> {
-        self.system.inc_block_number();
-        for extrinsic in extrinsics {
-            self.dispatch(extrinsic)?;
-        }
-        Ok(())
-    }
+	// Execute a block of extrinsics. Increments the block number.
+	fn execute_block(&mut self, extrinsics: Vec<Extrinsic>) -> Result<(), &'static str> {
+		self.system.inc_block_number();
+		for extrinsic in extrinsics {
+			self.dispatch(extrinsic)?;
+		}
+		Ok(())
+	}
 
-    fn dispatch(&mut self, extrinsic: Extrinsic) -> Result<(), &'static str> {
-        let Extrinsic { call, caller } = extrinsic;
-        self.system.inc_nonce(caller);
+	// Dispatch a specific extrinsic. Increments the user's nonce.
+	//
+	// Dispatch allows us to identify which underlying module call we want to execute.
+	// Note that we extract the `caller` from the extrinsic, and use that information
+	// to determine who we are executing the call on behalf of.
+	fn dispatch(&mut self, extrinsic: Extrinsic) -> Result<(), &'static str> {
+		let Extrinsic { call, caller } = extrinsic;
+		self.system.inc_nonce(caller);
 
-        match call {
-            RuntimeCall::Balances(balances::BalancesCall::Transfer { to, amount }) => {
-                self.balances.transfer(caller, to, amount)?;
-            }
-        }
+		match call {
+			RuntimeCall::Balances(balances::BalancesCall::Transfer { to, amount }) => {
+				self.balances.transfer(caller, to, amount)?;
+			},
+		}
 
-        Ok(())
-    }
+		Ok(())
+	}
 }

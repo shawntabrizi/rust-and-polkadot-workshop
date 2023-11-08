9b83d9ff5a86f8cb38acf911712624f734bf6ffe
Shawn Tabrizi <shawntabrizi@gmail.com>
2023-11-07 22:58:16 -0400

    more comments

--- code/src/balances.rs
+++ code/src/balances.rs
@@ -2,30 +2,43 @@ use core::fmt::Debug;
 use num::traits::{CheckedAdd, CheckedSub, Zero};
 use std::collections::BTreeMap;
 
+/// The configuration trait for the Balances Module.
+/// Contains the basic types needed for handling balances.
 pub trait Config: super::system::Config {
+	/// A type which can represent the balance of an account.
+	/// Usually this is a large unsigned integer.
 	type Balance: Zero + CheckedAdd + CheckedSub + Copy + Debug;
 }
 
-// This is the Balances Module.
-// It is a simple module which keeps track of how much balance each user has in this state machine.
+/// This is the Balances Module.
+/// It is a simple module which keeps track of how much balance each account has in this state
+/// machine.
 #[derive(Debug)]
 pub struct BalancesModule<T: Config> {
+	/// A map from account to the balance they have.
 	balances: BTreeMap<T::AccountId, T::Balance>,
 }
 
 impl<T: Config> BalancesModule<T> {
+	/// Create a new instance of the Balances Module.
 	pub fn new() -> Self {
 		Self { balances: BTreeMap::new() }
 	}
 
+	/// Set the balance of an account `who` to some `amount`.
 	pub fn set_balance(&mut self, who: T::AccountId, amount: T::Balance) {
 		self.balances.insert(who, amount);
 	}
 
+	/// Get the balance of an account `who`.
+	/// If the account has no stored balance, we return zero.
 	pub fn balance(&self, who: T::AccountId) -> T::Balance {
 		*self.balances.get(&who).unwrap_or(&T::Balance::zero())
 	}
 
+	/// Transfer `amount` from one another to another.
+	/// This function verifies that `from` has at least `amount` balance to transfer,
+	/// and that no mathematical overflows occur.
 	pub fn transfer(
 		&mut self,
 		from: T::AccountId,
@@ -52,6 +65,8 @@ pub enum BalancesCall<T: Config> {
 	Transfer { to: T::AccountId, amount: T::Balance },
 }
 
+/// Implementation of the dispatch logic, mapping from `BalancesCall` to the appropriate underlying
+/// function we want to execute.
 impl<T: Config> crate::support::Dispatch for BalancesModule<T> {
 	type Caller = T::AccountId;
 	type Call = BalancesCall<T>;
--- code/src/system.rs
+++ code/src/system.rs
@@ -5,36 +5,46 @@ use core::{
 use num::traits::{One, Zero};
 use std::collections::BTreeMap;
 
+/// The configuration trait for the System Module.
+/// This controls the common types used throughout our state machine.
 pub trait Config {
+	/// A type which can identify an account in our state machine.
+	/// On a real blockchain, you would want this to be a cryptographic public key.
 	type AccountId: Debug + Default + Ord + Copy;
+	/// A type which can be used to represent the current block number.
+	/// Usually a basic unsigned integer.
 	type BlockNumber: Debug + Default + One + Zero + AddAssign;
+	/// A type which can be used to keep track of the number of transactions from each account.
+	/// Usually a basic unsigned integer.
 	type Nonce: Debug + Default + One + Zero + Add + Copy;
 }
 
-// This is the System Module.
-// It handles low level meta information needed for your blockchain.
+/// This is the System Module.
+/// It handles low level state needed for your blockchain.
 #[derive(Default, Debug)]
 pub struct SystemModule<T: Config> {
+	/// The current block number.
 	block_number: T::BlockNumber,
+	/// A map from an account to their nonce.
 	nonce: BTreeMap<T::AccountId, T::Nonce>,
 }
 
-// The System Module is a low level system which is not really meant to be exposed to users.
-// Instead, these functions are used by your low level blockchain systems.
+/// The System Module is a low level system which is not really meant to be exposed to the outside
+/// world. Instead, these functions are used by your low level blockchain systems.
 impl<T: Config> SystemModule<T> {
-	// Create a new instance of the System Module.
+	/// Create a new instance of the System Module.
 	pub fn new() -> Self {
 		Self { block_number: Default::default(), nonce: Default::default() }
 	}
 
-	// This function can be used to increment the block number.
-	// Increases the block number by one.
+	/// This function can be used to increment the block number.
+	/// Increases the block number by one.
 	pub fn inc_block_number(&mut self) {
 		self.block_number += T::BlockNumber::one();
 	}
 
-	// Increment the nonce of a user. This helps us keep track of how many transactions each user
-	// has made.
+	/// Increment the nonce of an account. This helps us keep track of how many transactions each
+	/// account has made.
 	pub fn inc_nonce(&mut self, who: &T::AccountId) {
 		let nonce = *self.nonce.get(who).unwrap_or(&T::Nonce::zero());
 		let new_nonce = nonce + T::Nonce::one();
@@ -55,6 +65,8 @@ mod test {
 	#[test]
 	fn init_system() {
 		let mut system = super::SystemModule::<TestConfig>::new();
+		assert_eq!(system.block_number, 0);
+
 		system.inc_block_number();
 		system.inc_nonce(&"alice");
 

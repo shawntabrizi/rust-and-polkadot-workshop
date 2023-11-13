2715b1a0f59dbda17e4e87336dad720c065e5104
Shawn Tabrizi <shawntabrizi@gmail.com>
2023-11-12 22:11:55 -0400

    update macros

--- code/macros/src/call/expand.rs
+++ code/macros/src/call/expand.rs
@@ -1,25 +1,34 @@
 use super::parse::CallDef;
 use quote::quote;
 
-/// Expand definition, in particular:
-/// * add some bounds and variants to type defined,
-/// * create some new types,
-/// * impl stuff on them.
+/// See the `fn call` docs at the `lib.rs` of this crate for a high level definition.
 pub fn expand_call(def: CallDef) -> proc_macro2::TokenStream {
-	let CallDef { module_struct, methods } = def;
+	let CallDef { pallet_struct, methods } = def;
+
+	// This is a vector of all the callable function names.
 	let fn_name = methods.iter().map(|method| &method.name).collect::<Vec<_>>();
 
+	// This is a nested vector of all the arguments for each of the functions in `fn_name`. It does
+	// not include the `self` or `caller: T::AccountId` parameter, which we always assume are the
+	// first two parameters to these calls.
 	let args_name = methods
 		.iter()
 		.map(|method| method.args.iter().map(|(name, _)| name.clone()).collect::<Vec<_>>())
 		.collect::<Vec<_>>();
 
+	// This is a nested vector of all the types for all the arguments for each of the functions in
+	// `fn_name`. It has the same assumptions as `args_name`.
 	let args_type = methods
 		.iter()
 		.map(|method| method.args.iter().map(|(_, type_)| type_.clone()).collect::<Vec<_>>())
 		.collect::<Vec<_>>();
 
+	// This quote block creates an `enum Call` which contains all the calls exposed by our pallet,
+	// and the `Dispatch` trait logic to route a `caller` to access those functions.
 	let dispatch_impl = quote! {
+		// The callable functions exposed by this pallet.
+		//
+		// The parsed function names will be `snake_case`, and that will show up in the enum.
 		#[allow(non_camel_case_types)]
 		pub enum Call<T: Config> {
 			#(
@@ -27,7 +36,9 @@ pub fn expand_call(def: CallDef) -> proc_macro2::TokenStream {
 			)*
 		}
 
-		impl<T: Config> crate::support::Dispatch for #module_struct<T> {
+		// Dispatch logic at the pallet level, mapping each of the items in the `Call` enum to the
+		// appropriate function call with all arguments, including the `caller`.
+		impl<T: Config> crate::support::Dispatch for #pallet_struct<T> {
 			type Caller = T::AccountId;
 			type Call = Call<T>;
 
@@ -36,6 +47,7 @@ pub fn expand_call(def: CallDef) -> proc_macro2::TokenStream {
 					#(
 						Call::#fn_name { #( #args_name ),* } => {
 							self.#fn_name(
+								// Note that we assume the first argument of every call is the `caller`.
 								caller,
 								#( #args_name ),*
 							)?;
@@ -47,5 +59,6 @@ pub fn expand_call(def: CallDef) -> proc_macro2::TokenStream {
 		}
 	};
 
+	// Return the generated code.
 	dispatch_impl.into()
 }
--- code/macros/src/runtime/expand.rs
+++ code/macros/src/runtime/expand.rs
@@ -1,24 +1,25 @@
 use super::parse::RuntimeDef;
 use quote::quote;
 
-/// Expand definition, in particular:
-/// * add some bounds and variants to type defined,
-/// * create some new types,
-/// * impl stuff on them.
-pub fn expand_call(def: RuntimeDef) -> proc_macro2::TokenStream {
-	let RuntimeDef { runtime_struct, modules } = def;
+/// See the `fn runtime` docs at the `lib.rs` of this crate for a high level definition.
+pub fn expand_runtime(def: RuntimeDef) -> proc_macro2::TokenStream {
+	let RuntimeDef { runtime_struct, pallets } = def;
 
-	let module_names = modules.iter().map(|(name, _)| name.clone()).collect::<Vec<_>>();
-	let module_types = modules.iter().map(|(_, type_)| type_.clone()).collect::<Vec<_>>();
+	// This is a vector of all the pallet names, not including system.
+	let pallet_names = pallets.iter().map(|(name, _)| name.clone()).collect::<Vec<_>>();
+	// This is a vector of all the pallet types, not including system.
+	let pallet_types = pallets.iter().map(|(_, type_)| type_.clone()).collect::<Vec<_>>();
 
-	let dispatch_impl = quote! {
-		impl Runtime {
-			// Create a new instance of the main Runtime, by creating a new instance of each module.
+	// This quote block implements functions on the `Runtime` struct.
+	let runtime_impl = quote! {
+		impl #runtime_struct {
+			// Create a new instance of the main Runtime, by creating a new instance of each pallet.
 			fn new() -> Self {
 				Self {
-					system: <system::SystemModule::<Self>>::new(),
+					// Since system is not included in the list of pallets, we manually add it here.
+					system: <system::Pallet::<Self>>::new(),
 					#(
-						#module_names: <#module_types>::new()
+						#pallet_names: <#pallet_types>::new()
 					),*
 				}
 			}
@@ -40,12 +41,17 @@ pub fn expand_call(def: RuntimeDef) -> proc_macro2::TokenStream {
 				Ok(())
 			}
 		}
+	};
 
+	// This quote block implements the `RuntimeCall` enum and implements the `Dispatch` trait.
+	let dispatch_impl = quote! {
 		// These are all the calls which are exposed to the world.
-		// Note that it is just an accumulation of the calls exposed by each module.
+		// Note that it is just an accumulation of the calls exposed by each pallet.
+		//
+		// The parsed function names will be `snake_case`, and that will show up in the enum.
 		#[allow(non_camel_case_types)]
 		pub enum RuntimeCall {
-			#( #module_names(#module_names::Call<#runtime_struct>) ),*
+			#( #pallet_names(#pallet_names::Call<#runtime_struct>) ),*
 		}
 
 		impl crate::support::Dispatch for #runtime_struct {
@@ -53,7 +59,7 @@ pub fn expand_call(def: RuntimeDef) -> proc_macro2::TokenStream {
 			type Call = RuntimeCall;
 			// Dispatch a call on behalf of a caller. Increments the caller's nonce.
 			//
-			// Dispatch allows us to identify which underlying module call we want to execute.
+			// Dispatch allows us to identify which underlying pallet call we want to execute.
 			// Note that we extract the `caller` from the extrinsic, and use that information
 			// to determine who we are executing the call on behalf of.
 			fn dispatch(
@@ -63,10 +69,12 @@ pub fn expand_call(def: RuntimeDef) -> proc_macro2::TokenStream {
 			) -> Result<(), &'static str> {
 				self.system.inc_nonce(&caller);
 
+				// This match statement will allow us to correctly route `RuntimeCall`s
+				// to the appropriate pallet level call.
 				match runtime_call {
 					#(
-						RuntimeCall::#module_names(call) => {
-							self.#module_names.dispatch(caller, call)?;
+						RuntimeCall::#pallet_names(call) => {
+							self.#pallet_names.dispatch(caller, call)?;
 						}
 					),*
 				}
@@ -75,5 +83,10 @@ pub fn expand_call(def: RuntimeDef) -> proc_macro2::TokenStream {
 		}
 	};
 
-	dispatch_impl.into()
+	// We combine and return all the generated code.
+	quote! {
+		#dispatch_impl
+		#runtime_impl
+	}
+	.into()
 }
